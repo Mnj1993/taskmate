a408712f73806943221843137ecf77f1ac7b7661
Manoj Nagarajan <manoj.nagarajan1993@gmail.com>
2021-07-07 15:18:28 -0400

    Reupload

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
@@ -122,6 +123,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'todolist'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
